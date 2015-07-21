3d358c1fb6113c265a089ec4351b681dd902e455
Jenan Mannette <jenanmannette@gmail.com>
2015-07-21 10:45:09 -0600

    Render books from NYT API

--- app.js
+++ app.js
@@ -29,9 +29,12 @@ app.use('/users', users);
 
 app.get('/books', function(req, res) {
     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
-      .end(function (response) {
-        console.log(response.body);
-      })
+    .end(function (response) {
+      var NYTBooks = response.body.results.books;
+      res.render('index', {books: NYTBooks});
+      // console.log(response.body.results.books);
+      // res.end('Done')
+    })
 })
 
 // catch 404 and forward to error handler
